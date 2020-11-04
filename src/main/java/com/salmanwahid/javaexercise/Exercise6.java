2f26811bb0dcce39c9fb1b4d586e1f207df79625
Salman Wahid <swchowdhury10@gmail.com>
2020-11-04 20:28:03 +0530

    Exercise6

---
+++ src/main/java/com/salmanwahid/javaexercise/Exercise6.java
@@ -0,0 +1,15 @@
+package com.salmanwahid.javaexercise;
+
+public class Exercise6 {
+	public static void main(String[] args) {
+		
+		int num1 = 125;
+		int num2 = 24;
+		System.out.println(num1 + " + " +num2+ " = " + (num1 + num2));
+		System.out.println(num1 + " - " +num2+ " = " + (num1 - num2));
+		System.out.println(num1+ " * " +num2+ " = " + (num1 * num2));
+		System.out.println(num1+" / "+num2+ " = " + (num1 / num2));
+		System.out.println(num1+" mod "+num2+ " = " + (num1% num2));
+		
+	}
+}
