a655da080a36adee276373aed773a94e25176b23
Salman Wahid <swchowdhury10@gmail.com>
2021-01-16 21:49:28 +0530

    Java IFelse statements

---
+++ src/main/java/com/salmanwahid/learningjava/javaTpoint/NestedifExample.java
@@ -0,0 +1,14 @@
+package com.salmanwahid.learningjava.javaTpoint;
+
+public class NestedifExample {
+	public static void main(String[] args) {
+		int age=20;
+		int weight=65;
+		if (age>=18) {
+			if (weight>50) {
+				System.out.println("You are eligible to donate BLOOD");
+			}
+		}
+	}
+
+}
